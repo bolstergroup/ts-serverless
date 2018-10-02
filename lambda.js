5a44a6fd88ed1a6c17716b95a616d70cf376ee3c
Sam La Salle <sam@lasalle.id.au>
2018-10-02 16:20:23 +1000

    Add next and react to SSR everything in pages/

---
+++ lambda.js
@@ -0,0 +1,8 @@
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./server');
+
+const binaryMimeTypes = ['*/*'];
+
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
+
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
