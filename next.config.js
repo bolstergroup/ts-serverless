2d9109de9a16eb34543d9cd6dd5faa26df3d32d1
Sam La Salle <sam@lasalle.id.au>
2018-10-04 11:36:01 +1000

    Add typescript support

--- next.config.js
+++ next.config.js
@@ -1,16 +1,13 @@
 const path = require('path');
 const withSass = require('@zeit/next-sass');
+const withTypescript = require('@zeit/next-typescript');
 
 require('dotenv').config({ path: path.resolve(__dirname, `./env/${process.env.ENV_FILE}`) });
 
 const { STATIC_PATH } = process.env;
 
-module.exports = Object.assign(
-  {},
-  withSass(),
-  {
-    publicRuntimeConfig: {
-      STATIC_PATH,
-    },
+module.exports = withTypescript(withSass({
+  publicRuntimeConfig: {
+    STATIC_PATH,
   }
-);
+}));
